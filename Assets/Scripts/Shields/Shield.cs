748478356822ccdc0f08a6e0f63439a0bb8ca2bc
Zach <zacharyfejes@gmail.com>
2018-03-05 01:22:51 -0500

    Created shield manager system, shield class, and shield effects #6

--- Assets/Scripts/Shields/Shield.cs
+++ Assets/Scripts/Shields/Shield.cs
@@ -1,16 +1,58 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shield : MonoBehaviour {
 
-	public GameObject shieldFlarePrefab;
+    private ShieldManager shieldManager;
+    private Collider shieldCollider;
+    private MeshRenderer shieldRenderer;
+    public float maxShieldHP = 100.0f;
+    private float shieldHP;
+    private bool shieldActive = true;
 
-	void OnCollisionEnter (Collision collision)
-    {
-        ContactPoint contact = collision.contacts[0];
-        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
-        Vector3 pos = contact.point;
-        Instantiate(shieldFlarePrefab, pos, rot);
+    void Start() {
+        shieldHP = maxShieldHP;
+        shieldCollider = transform.GetComponent<Collider>();
+        shieldRenderer = transform.GetComponent<MeshRenderer>();
+    }
+
+    public void SetShieldManager(ShieldManager newShieldManager) {
+        shieldManager = newShieldManager;
+    }
+
+    public float GetShieldHP() {
+        return(shieldHP);
+    }
+
+    public void DamageShield(float damage) {
+        if(shieldHP - damage <= 0) {
+            shieldHP = 0;
+            DeactivateShield();
+        }
+        else {
+            shieldHP -= damage;
+        }
+    }
+
+    public void HealShield(float healing) {
+        if(shieldHP + healing >= maxShieldHP) {
+            shieldHP = maxShieldHP;
+        }
+        else {
+            shieldHP += healing;
+        }
+    }
+
+    public void ActivateShield() {
+        shieldActive = true;
+        shieldCollider.enabled = true;
+        shieldRenderer.enabled = true;
+    }
+
+    public void DeactivateShield() {
+        shieldActive = false;
+        shieldCollider.enabled = false;
+        shieldRenderer.enabled = false;
     }
 }
