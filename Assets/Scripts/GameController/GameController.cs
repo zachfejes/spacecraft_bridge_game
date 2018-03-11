7d489f438b3e0003710be3f31ffbd4f6890529f5
Zach <zacharyfejes@gmail.com>
2018-03-10 20:11:17 -0500

    GameController now spawns player and enemies #20

--- Assets/Scripts/GameController/GameController.cs
+++ Assets/Scripts/GameController/GameController.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject playerShipPrefab;
+	public GameObject enemyShipPrefab;
+
+	public GameObject spacecraftParent;
+	public Transform playerSpawnPoint;
+	public Transform[] enemySpawnPoints;
+
+	public int numberOfEnemies = 0;
+
+	public GameObject playerShip;
+	public List<GameObject> enemyShips = new List<GameObject>();
+
+
+	void Awake() {
+		InitializeGame();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	public void InitializeGame() {
+		playerShip = GameObject.Instantiate(playerShipPrefab, playerSpawnPoint.position, Quaternion.Euler(Random.Range(-180,180), Random.Range(-180,180),Random.Range(-180,180)));
 		
+		for (int i = 0; i < numberOfEnemies; i++) {
+			Vector3 spawnPosition = enemySpawnPoints[Random.Range(0, enemySpawnPoints.Length - 1)].position + new Vector3(Random.Range(-10, 10), Random.Range(-10,10), Random.Range(-10,10));
+			Quaternion spawnRotation = Quaternion.Euler(Random.Range(-180,180), Random.Range(-180,180),Random.Range(-180,180));
+
+			enemyShips.Add(GameObject.Instantiate(enemyShipPrefab, spawnPosition, spawnRotation));
+		}
 	}
+
+
 }
